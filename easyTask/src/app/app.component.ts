0103ff41349a294e05d34a092d5a22feb07c9fa9
Abhilash Kelageri <abhiabhilash74658.com>
2025-01-28 20:29:31 +0530

    Added @input

--- easyTask/src/app/app.component.ts
+++ easyTask/src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   users = DUMMY_USERS;
+  selectedUserId = 'u3';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string) {
-    console.log("User id selected " + id);
+    this.selectedUserId = id;
   }
 }
