e2f9cac26bf2380e9bad61900d5dac31d00ac716
Abhilash Kelageri <abhiabhilash74658.com>
2025-02-09 00:26:46 +0530

    Added new task component

--- easyTask/src/app/tasks/new-task/new-task.component.ts
+++ easyTask/src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskAdded } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskAdded>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onCancelAddtask() {
+    this.cancel.emit();
+  }
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    });
+  }
 
 }
--- easyTask/src/app/tasks/tasks.component.ts
+++ easyTask/src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskAdded } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isTaskAdded = false;
   tasks = [
     {
       id: 't1',
@@ -43,5 +46,24 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onTaskAdded() {
+    this.isTaskAdded = true;
+  }
+
+  onCancelAddTask() {
+    this.isTaskAdded = false;
+  }
+
+  onSubmitTask(taskData: NewTaskAdded) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
+    this.isTaskAdded = false;
   }
 }
